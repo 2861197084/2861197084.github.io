901bfaaf7f91cc88611bd6e375a70d4eacd1889e
2861197084 <2861197084@qq.com>
2024-09-27 00:29:07 +0800

    Site updated: 2024-09-27 00:29:00

--- js/fomal.js
+++ js/fomal.js
@@ -3178,8 +3178,8 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
-    --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
+    --default-bg: url(assets/default.jpg);
+    --darkmode-bg:url(assets/dark.jpg);
     --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
     --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
   }`;
